3cf7a0ada3f12e9124255f6c06bacefa7312cbe1
CodexAdrian <83074853+CodexAdrian@users.noreply.github.com>
2022-01-09 18:43:06 -0500

    Added watering can + functionality

---
+++ src/main/java/com/toadstoolstudios/sprout/items/WateringCanItem.java
@@ -0,0 +1,102 @@
+package com.toadstoolstudios.sprout.items;
+
+import net.minecraft.block.*;
+import net.minecraft.entity.player.PlayerEntity;
+import net.minecraft.item.Item;
+import net.minecraft.item.ItemStack;
+import net.minecraft.item.ItemUsageContext;
+import net.minecraft.particle.ParticleEffect;
+import net.minecraft.particle.ParticleTypes;
+import net.minecraft.server.world.ServerWorld;
+import net.minecraft.stat.Stats;
+import net.minecraft.state.property.Properties;
+import net.minecraft.util.ActionResult;
+import net.minecraft.util.Hand;
+import net.minecraft.util.TypedActionResult;
+import net.minecraft.util.hit.BlockHitResult;
+import net.minecraft.util.hit.HitResult;
+import net.minecraft.util.math.BlockPos;
+import net.minecraft.util.math.Box;
+import net.minecraft.util.math.MathHelper;
+import net.minecraft.world.RaycastContext;
+import net.minecraft.world.World;
+
+public class WateringCanItem extends Item {
+    public WateringCanItem(Settings settings) {
+        super(settings);
+    }
+
+    @Override
+    public TypedActionResult<ItemStack> use(World world, PlayerEntity user, Hand hand) {
+        BlockHitResult blockHitResult = raycast(world, user, RaycastContext.FluidHandling.SOURCE_ONLY);
+        BlockPos blockPos = blockHitResult.getBlockPos();
+        ItemStack itemStack = user.getStackInHand(hand);
+        if (blockHitResult.getType() == HitResult.Type.MISS) {
+            return TypedActionResult.pass(itemStack);
+        }
+        if (blockHitResult.getType() == HitResult.Type.BLOCK) {
+            BlockState blockState = world.getBlockState(blockPos);
+            if (blockState.getBlock() instanceof FluidDrainable && blockState.isOf(Blocks.WATER)) {
+                user.incrementStat(Stats.USED.getOrCreateStat(this));
+                itemStack.getOrCreateNbt().putInt("Water", 100);
+                return TypedActionResult.success(itemStack, world.isClient());
+            }
+            return TypedActionResult.pass(itemStack);
+        }
+        return super.use(world, user, hand);
+    }
+
+    @Override
+    public ActionResult useOnBlock(ItemUsageContext context) {
+        var block = context.getBlockPos();
+        Box blockBox = new Box(block).expand(1, 1, 1);
+        World world = context.getWorld();
+        ItemStack stack = context.getStack();
+        int water = getWater(stack);
+        System.out.println(water);
+        if (water > 0) {
+            if (world instanceof ServerWorld serverWorld) {
+                setWater(stack, water - 1);
+                BlockPos.stream(blockBox.shrink(0, 1, 0)).forEach(blockPos -> serverWorld.spawnParticles(ParticleTypes.SPLASH, blockPos.getX(), blockPos.getY(), blockPos.getZ(), 1, 0, 0, 0, 1.4));
+                BlockPos.stream(blockBox).filter(blockPos -> world.getBlockState(blockPos).getBlock() instanceof Fertilizable).forEach(blockPos -> {
+                    if (world.getRandom().nextInt(25) == 0) {
+                        BlockState crop = world.getBlockState(blockPos);
+                        Fertilizable fertilizable = ((Fertilizable) crop.getBlock());
+                        fertilizable.grow(serverWorld, world.random, blockPos, crop);
+                    }
+                });
+                BlockPos.stream(blockBox).filter(blockPos -> world.getBlockState(blockPos).getBlock() instanceof FarmlandBlock).forEach(blockPos -> {
+                    BlockState blockState = serverWorld.getBlockState(blockPos);
+                    int moisture = blockState.get(Properties.MOISTURE);
+                    if(world.getRandom().nextInt(15) == 0 && moisture < 7){
+                        world.setBlockState(blockPos, blockState.with(Properties.MOISTURE, 7));
+                    }
+                });
+            }
+        }
+        return super.useOnBlock(context);
+    }
+
+    @Override
+    public boolean isItemBarVisible(ItemStack stack) {
+        return true;
+    }
+
+    @Override
+    public int getItemBarStep(ItemStack stack) {
+        return (int) (getWater(stack) / 100F * 13);
+    }
+
+    @Override
+    public int getItemBarColor(ItemStack stack) {
+        return MathHelper.packRgb(0.4f, 0.4f, 1.0f);
+    }
+
+    public int getWater(ItemStack stack) {
+        return stack.getNbt() != null ? stack.getNbt().getInt("Water") : 0;
+    }
+
+    public void setWater(ItemStack stack, int amount) {
+        stack.getOrCreateNbt().putInt("Water", amount);
+    }
+}
--- src/main/java/com/toadstoolstudios/sprout/registry/SproutItems.java
+++ src/main/java/com/toadstoolstudios/sprout/registry/SproutItems.java
@@ -1,6 +1,7 @@
 package com.toadstoolstudios.sprout.registry;
 
 import com.toadstoolstudios.sprout.Sprout;
+import com.toadstoolstudios.sprout.items.WateringCanItem;
 import net.fabricmc.fabric.api.item.v1.FabricItemSettings;
 import net.minecraft.item.Item;
 import net.minecraft.item.SpawnEggItem;
@@ -12,6 +13,7 @@ import static com.toadstoolstudios.sprout.Sprout.MOD_ID;
 public class SproutItems {
     public static final Item ELEPHANT_SPAWN_EGG = new SpawnEggItem(SproutEntities.ELEPHANT_ENTITY_TYPE, 0x8198a0,0x52556c, genericSettings());
     public static final Item PEANUT = new Item(genericSettings());
+    public static final Item WATERING_CAN = new WateringCanItem(genericSettings().maxCount(1));
     private static FabricItemSettings genericSettings() {
         return new FabricItemSettings().group(Sprout.SPROUT_TAB);
     }
@@ -19,5 +21,6 @@ public class SproutItems {
     public static void registerItems() {
         Registry.register(Registry.ITEM, new Identifier(MOD_ID, "elephant_spawn_egg"), ELEPHANT_SPAWN_EGG);
         Registry.register(Registry.ITEM, new Identifier(MOD_ID, "peanut"), PEANUT);
+        Registry.register(Registry.ITEM, new Identifier(MOD_ID, "watering_can"), WATERING_CAN);
     }
 }
