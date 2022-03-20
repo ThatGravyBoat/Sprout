4b0e267e118f7bb32fa572666ff02e252ee95257
CodexAdrian <83074853+CodexAdrian@users.noreply.github.com>
2022-03-20 14:34:50 -0400

    Initial commit

---
+++ forge/src/main/java/net/examplemod/forge/ExampleModForge.java
@@ -0,0 +1,12 @@
+package net.examplemod.forge;
+
+import net.examplemod.ExampleMod;
+import net.minecraftforge.fml.common.Mod;
+import net.minecraftforge.fml.javafmlmod.FMLJavaModLoadingContext;
+
+@Mod(ExampleMod.MOD_ID)
+public class ExampleModForge {
+    public ExampleModForge() {
+        ExampleMod.init();
+    }
+}
